533fdfae110b6b799e30ddf6192247fccc326aeb
nokkya94 <tanasiev94@yahoo.com>
2025-08-17 17:19:39 -0700

    Refactor backend configuration and update variable definitions for clarity and consistency

--- backend.tf
+++ backend.tf
@@ -1,117 +1,117 @@
-# terraform {
-#    backend "s3" {
-#     bucket         = "my-terraform-state-bucket-7748123"
-#     key            = "state/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock-table-7748123"
-#     encrypt        = true
-#   }
-# }
+terraform {
+   backend "s3" {
+    bucket         = "my-terraform-state-bucket-7748123"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table-7748123"
+    encrypt        = true
+  }
+}
 
-# resource "aws_s3_bucket" "tf_state" {
-#   bucket = var.backend_bucket_name
-#   force_destroy = true
-#   tags = {
-#     Name = "TerraformStateBucket"
-#   }
-# }
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.backend_state_bucket_name
+  force_destroy = true
+  tags = {
+    Name = "TerraformStateBucket"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
-#   bucket = aws_s3_bucket.tf_state.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm     = "aws:kms"
-#       kms_master_key_id = aws_kms_key.terraform_state_key.arn
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.terraform_state_key.arn
+    }
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-#   bucket = aws_s3_bucket.tf_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_policy" "tf_state_policy" {
-#   bucket = aws_s3_bucket.tf_state.id
+resource "aws_s3_bucket_policy" "tf_state_policy" {
+  bucket = aws_s3_bucket.tf_state.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "*"
-#         }
-#         Action = [
-#           "s3:GetObject",
-#           "s3:PutObject",
-#           "s3:DeleteObject",
-#           "s3:ListBucket"
-#         ]
-#         Resource = [
-#           aws_s3_bucket.tf_state.arn,
-#           "${aws_s3_bucket.tf_state.arn}/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.tf_state.arn,
+          "${aws_s3_bucket.tf_state.arn}/*"
+        ]
+      }
+    ]
+  })
+}
 
-# resource "aws_kms_key" "terraform_state_key" {
-#   description             = "KMS key for Terraform state bucket encryption"
-#   deletion_window_in_days = 10
-#   enable_key_rotation     = true
+resource "aws_kms_key" "terraform_state_key" {
+  description             = "KMS key for Terraform state bucket encryption"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Id = "terraform-kms-policy"
-#     Statement = [
-#       {
-#         Sid = "Allow account access"
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#         }
-#         Action = [
-#           "kms:*"
-#         ]
-#         Resource = "*"
-#       },
-#       {
-#         Sid = "Allow Terraform IAM role or user access"
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.iam_role_name}"
-#         }
-#         Action = [
-#           "kms:Encrypt",
-#           "kms:Decrypt",
-#           "kms:GenerateDataKey*",
-#           "kms:DescribeKey"
-#         ]
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id = "terraform-kms-policy"
+    Statement = [
+      {
+        Sid = "Allow account access"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "kms:*"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid = "Allow Terraform IAM role or user access"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.iam_role_name}"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
 
-# resource "aws_dynamodb_table" "tf_lock" {
-#   name         = var.dynamodb_table_name
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.dynamodb_state_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-#   tags = {
-#     Name = "TerraformLockTable"
-#   }
-# }
+  tags = {
+    Name = "TerraformLockTable"
+  }
+}
