02e6fd0bdc44f0ebf502b4ea1aa9874ec4ef7b78
nokkya94 <tanasiev94@yahoo.com>
2025-08-20 13:57:29 -0700

    Update PostgreSQL engine version to 14.17 in RDS instance configuration

--- modules/rds_postgres/rds_postgres.tf
+++ modules/rds_postgres/rds_postgres.tf
@@ -29,7 +29,7 @@ resource "aws_db_parameter_group" "webapp_postgres" {
 resource "aws_db_instance" "webapp" {
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "14.13"
+  engine_version       = "14.17"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
   username             = var.db_username
